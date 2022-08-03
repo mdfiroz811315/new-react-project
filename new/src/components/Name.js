2e7de67cf83dba44577606a7d8cda9fee45de770
md <mdfiroz811315@gmail.com>
2022-08-04 01:21:19 +0530

    new adding code

--- new/src/components/Name.js
+++ new/src/components/Name.js
@@ -1,6 +1,12 @@
+
 function Name() {
     return (
+        <>
         <div className="centertext"> I am in Name page</div>
+        <img style={{ position: 'absolute', left: '50px' }}src="img_tree.png"></img>
+        </>
+        
+        
   )
   }
   
--- new/src/components/Table.js
+++ new/src/components/Table.js
@@ -2,13 +2,15 @@ import Footer from "./Footer";
 function Table() {
     return (
         <div>
-            <h2> Table Form</h2>
-           <form>
+            <h2> Text field</h2>
+           <form action="/action_page.html">
                 <label for="fname">First name:</label><br></br>
-                <input type="text" id="fname" name="fname" value="John" /><br></br>
+                <input type="text" id="fname" name="fname" value="Mirza" /><br></br>
                 <label for="lname">Last name:</label><br></br>
-                <input type="text" id="lname" name="lname" value="Doe" /><br></br><br></br>
-                <input type="submit" value="Submit" />
+                <input type="text" id="lname" name="lname" value="Firoz" /><br></br>
+                <label type="mmail" >Mail id:</label><br></br>
+                <input type="text" id="mname" mail="mmail" value="email id" /><br></br>
+                <input type="submit" value="Submit"/>
             </form> 
         </div>
   )
